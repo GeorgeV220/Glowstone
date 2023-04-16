4e51540c719703da27eff0d3a629fa361ff87be8
George V <georgev22.github@gmail.com>
2023-04-17 01:12:22 +0300

    Added PlayerClientSettingsEvent

--- src/main/java/net/glowstone/net/handler/play/game/ClientSettingsHandler.java
+++ src/main/java/net/glowstone/net/handler/play/game/ClientSettingsHandler.java
@@ -1,15 +1,21 @@
 package net.glowstone.net.handler.play.game;
 
 import com.flowpowered.network.MessageHandler;
+import net.glowstone.EventFactory;
 import net.glowstone.entity.meta.ClientSettings;
+import net.glowstone.events.player.PlayerClientSettingsEvent;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.game.ClientSettingsMessage;
 
 public final class ClientSettingsHandler implements
-    MessageHandler<GlowSession, ClientSettingsMessage> {
+        MessageHandler<GlowSession, ClientSettingsMessage> {
 
     @Override
     public void handle(GlowSession session, ClientSettingsMessage message) {
-        session.getPlayer().setSettings(new ClientSettings(message));
+        PlayerClientSettingsEvent event = new PlayerClientSettingsEvent(session, new ClientSettings(message));
+        EventFactory.getInstance().callEvent(event);
+        if (!event.isCancelled()) {
+            event.getGlowPlayer().setSettings(event.getClientSettings());
+        }
     }
 }
