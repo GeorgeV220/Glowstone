80f03faf2e1d84699cc687b56bacb75a9cd57c5a
George V <georgev22.github@gmail.com>
2023-04-17 05:02:57 +0300

    Added functionality to retrieve glowstone.yml from jar file for plugin loading

--- src/main/java/net/glowstone/util/linkstone/LinkstonePluginLoader.java
+++ src/main/java/net/glowstone/util/linkstone/LinkstonePluginLoader.java
@@ -7,15 +7,23 @@ import net.glowstone.linkstone.runtime.inithook.ClassInitInvokeVisitor;
 import net.glowstone.linkstone.runtime.reflectionredirect.field.FieldAccessorUtility;
 import net.glowstone.linkstone.runtime.reflectionredirect.method.MethodAccessorUtility;
 import net.glowstone.linkstone.runtime.reflectionreplace.ReflectionReplaceVisitor;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Server;
+import org.bukkit.plugin.InvalidDescriptionException;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 import org.bukkit.plugin.java.PluginClassLoader;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class LinkstonePluginLoader extends JavaPluginLoader {
     /**
@@ -28,7 +36,7 @@ public class LinkstonePluginLoader extends JavaPluginLoader {
         super(instance);
         LinkstoneRuntimeData.setPluginClassLoader(new ClassLoader() {
             @Override
-            protected Class<?> findClass(String name) throws ClassNotFoundException {
+            protected Class<?> findClass(String name) {
                 return getClassByName(name);
             }
         });
@@ -36,7 +44,7 @@ public class LinkstonePluginLoader extends JavaPluginLoader {
 
     @Override
     protected PluginClassLoader newPluginLoader(JavaPluginLoader loader, ClassLoader parent,
-            PluginDescriptionFile description, File dataFolder, File file) throws Exception {
+                                                PluginDescriptionFile description, File dataFolder, File file) throws Exception {
         return new PluginClassLoader(loader, parent, description, dataFolder, file) {
             @Override
             protected byte[] transformBytecode(byte[] bytecode) {
@@ -60,5 +68,42 @@ public class LinkstonePluginLoader extends JavaPluginLoader {
                 return cw.toByteArray();
             }
         };
+    }
+
+    @Override
+    public PluginDescriptionFile getPluginDescription(File file) throws InvalidDescriptionException {
+        Validate.notNull(file, "File cannot be null");
+
+        JarFile jar = null;
+        InputStream stream = null;
+
+        try {
+            jar = new JarFile(file);
+            JarEntry entry = jar.getJarEntry("glowstone.yml") != null ? jar.getJarEntry("glowstone.yml") : jar.getJarEntry("plugin.yml");
+
+            if (entry == null) {
+                throw new InvalidDescriptionException(new FileNotFoundException("Jar does not contain glowstone.yml or plugin.yml"));
+            }
+
+            stream = jar.getInputStream(entry);
+
+            return new PluginDescriptionFile(stream);
+
+        } catch (IOException | YAMLException ex) {
+            throw new InvalidDescriptionException(ex);
+        } finally {
+            if (jar != null) {
+                try {
+                    jar.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
     }
 }
